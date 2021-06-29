6b1861e84c106654a2be85ea075dcc558e3dd0e5
daiki823 <waiht589@gmail.com>
2021-06-29 22:11:41 +0900

    contents_dbに学校、クラス、アカウントテーブルのselect分とりあえず追加

--- assets/php/contents_db.php
+++ assets/php/contents_db.php
@@ -10,10 +10,11 @@ require_once("pdointerface.php");
 ////////////////////////////////////
 //以下、DBクラス使用例
 
+
 //--------------------------------------------------------------------------------------
-///	都道府県クラス
+///	学校クラス
 //--------------------------------------------------------------------------------------
-class cprefecture extends crecord {
+class cschool extends crecord {
 	//--------------------------------------------------------------------------------------
 	/*!
 	@brief	コンストラクタ
@@ -30,12 +31,12 @@ class cprefecture extends crecord {
 	@return	個数
 	*/
 	//--------------------------------------------------------------------------------------
-	public function get_all_count($debug){
+	public function get_all_count($debug=false){
 		//親クラスのselect()メンバ関数を呼ぶ
 		$this->select(
 			$debug,					//デバッグ文字を出力するかどうか
 			"count(*)",				//取得するカラム
-			"prefecture",			//取得するテーブル
+			"school",			//取得するテーブル
 			"1"					//条件
 		);
 		if($row = $this->fetch_assoc()){
@@ -49,23 +50,19 @@ class cprefecture extends crecord {
 	//--------------------------------------------------------------------------------------
 	/*!
 	@brief	指定された範囲の配列を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@param[in]	$from	抽出開始行
-	@param[in]	$limit	抽出数
+	@param[in]	$column　取得するデータ
 	@return	配列（2次元配列になる）
 	*/
 	//--------------------------------------------------------------------------------------
-	public function get_all($debug,$from,$limit){
+	public function get_all($column){
 		$arr = array();
 		//親クラスのselect()メンバ関数を呼ぶ
 		$this->select(
-			$debug,			//デバッグ表示するかどうか
-			"*",			//取得するカラム
-			"prefecture",	//取得するテーブル
-			"1",			//条件
-			"prefecture_id asc",	//並び替え
-			"limit " . $from . "," . $limit		//抽出開始行と抽出数
-		);
+			false,			//デバッグ表示するかどうか
+			$column,			//取得するカラム
+			"school",	//取得するテーブル
+			"1"			//条件
+			);
 		//順次取り出す
 		while($row = $this->fetch_assoc()){
 			$arr[] = $row;
@@ -73,96 +70,36 @@ class cprefecture extends crecord {
 		//取得した配列を返す
 		return $arr;
 	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	指定された範囲の配列を得る（プレースホルダつき版）
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@param[in]	$from	抽出開始行
-	@param[in]	$limit	抽出数
-	@return	配列（2次元配列になる）
-	*/
-	//--------------------------------------------------------------------------------------
-	public function get_all_prep($debug,$from,$limit){
-		$arr = array();
-		$query = <<< END_BLOCK
-select
-*
-from
-prefecture
-where
-1
-order by
-prefecture_id asc
-limit ?, ?
-END_BLOCK;
-		$prep_arr = array($from,$limit);
-		//親クラスのselect_query()メンバ関数を呼ぶ
-		$this->select_query(
-			$debug,			//デバッグ表示するかどうか
-			$query,			//プレースホルダつきSQL
-			$prep_arr		//データの配列
-		);
-		//順次取り出す
-		while($row = $this->fetch_assoc()){
-			$arr[] = $row;
-		}
-		//取得した配列を返す
-		return $arr;
-	}
+
 	//--------------------------------------------------------------------------------------
 	/*!
 	@brief	指定されたIDの配列を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
 	@param[in]	$id		ID
 	@return	配列（1次元配列になる）空の場合はfalse
 	*/
 	//--------------------------------------------------------------------------------------
-	public function get_tgt($debug,$id){
-		if(!is_int($id)
+	public function get_tgt($id){
+		if(!cutil::is_number($id)
 		||  $id < 1){
 			//falseを返す
 			return false;
 		}
 		//親クラスのselect()メンバ関数を呼ぶ
 		$this->select(
-			$debug,			//デバッグ表示するかどうか
+			false,			//デバッグ表示するかどうか
 			"*",			//取得するカラム
-			"prefecture",	//取得するテーブル
-			"prefecture_id=" . $id	//条件
+			"school",	//取得するテーブル
+			"school_id=" . $id	//条件
 		);
-		return $this->fetch_assoc();
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	指定されたIDの配列を得る（プレースホルダつき版）
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@param[in]	$id		ID
-	@return	配列（1次元配列になる）空の場合はfalse
-	*/
-	//--------------------------------------------------------------------------------------
-	public function get_tgt_prep($debug,$id){
-		if(!is_int($id)
-		||  $id < 1){
-			//falseを返す
-			return false;
+		//順次取り出す
+		while($row = $this->fetch_assoc()){
+			$arr[] = $row;
 		}
-		$query = <<< END_BLOCK
-select
-*
-from
-prefecture
-where
-prefecture_id = ?
-END_BLOCK;
-		$prep_arr = array($id);
-		//親クラスのselect_query()メンバ関数を呼ぶ
-		$this->select_query(
-			$debug,			//デバッグ表示するかどうか
-			$query,			//プレースホルダつきSQL
-			$prep_arr		//データの配列
-		);
-		return $this->fetch_assoc();
+		//取得した配列を返す
+		return $arr;
+
 	}
+
 	//--------------------------------------------------------------------------------------
 	/*!
 	@brief	デストラクタ
@@ -174,10 +111,8 @@ END_BLOCK;
 	}
 }
 
-//--------------------------------------------------------------------------------------
-///	フルーツクラス
-//--------------------------------------------------------------------------------------
-class cfruits extends crecord {
+//クラスクラス
+class cclass extends crecord {
 	//--------------------------------------------------------------------------------------
 	/*!
 	@brief	コンストラクタ
@@ -189,84 +124,6 @@ class cfruits extends crecord {
 	}
 	//--------------------------------------------------------------------------------------
 	/*!
-	@brief	すべての個数を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@return	個数
-	*/
-	//--------------------------------------------------------------------------------------
-	public function get_all_count($debug){
-		return $this->get_all_count_simple($debug,'fruits');
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	指定された範囲の配列を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@return	配列（2次元配列になる）
-	*/
-	//--------------------------------------------------------------------------------------
-	public function get_all($debug){
-		return $this->get_alltable_simple($debug,'fruits','fruits_id');
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	指定されたIDの配列を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@param[in]	$id		ID
-	@return	配列（1次元配列になる）空の場合はfalse
-	*/
-	//--------------------------------------------------------------------------------------
-	public function get_tgt($debug,$id){
-		return $this->get_tgt_simple($debug,$id,'fruits','fruits_id');
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	デストラクタ
-	*/
-	//--------------------------------------------------------------------------------------
-	public function __destruct(){
-		//親クラスのデストラクタを呼ぶ
-		parent::__destruct();
-	}
-}
-
-//--------------------------------------------------------------------------------------
-///	メンバークラス
-//--------------------------------------------------------------------------------------
-class cmember extends crecord {
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	コンストラクタ
-	*/
-	//--------------------------------------------------------------------------------------
-	public function __construct() {
-		//親クラスのコンストラクタを呼ぶ
-		parent::__construct();
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	すべての個数を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@return	個数
-	*/
-	//--------------------------------------------------------------------------------------
-	public function get_all_count($debug){
-		//親クラスのselect()メンバ関数を呼ぶ
-		$this->select(
-			$debug,					//デバッグ文字を出力するかどうか
-			"count(*)",				//取得するカラム
-			"member,prefecture",			//取得するテーブル
-			"member.prefecture_id = prefecture.prefecture_id" //条件
-		);
-		if($row = $this->fetch_assoc()){
-			//取得した個数を返す
-			return $row['count(*)'];
-		}
-		else{
-			return 0;
-		}
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
 	@brief	指定された範囲の配列を得る
 	@param[in]	$debug	デバッグ出力をするかどうか
 	@param[in]	$from	抽出開始行
@@ -274,16 +131,16 @@ class cmember extends crecord {
 	@return	配列（2次元配列になる）
 	*/
 	//--------------------------------------------------------------------------------------
-	public function get_all($debug,$from,$limit){
+	public function get_all($column="class.*"){
 		$arr = array();
 		//親クラスのselect()メンバ関数を呼ぶ
 		$this->select(
-			$debug,			//デバッグ表示するかどうか
-			"member.*,prefecture.prefecture_name", //取得するカラム
-			"member,prefecture",	//取得するテーブル
-			"member.prefecture_id = prefecture.prefecture_id", //条件
-			"member.member_id asc",	//並び替え
-			"limit " . $from . "," . $limit		//抽出開始行と抽出数
+			false,			//デバッグ表示するかどうか
+			$column,		//取得するカラム
+			"class,school",			//取得するテーブル
+			"class.school_id = school.school_id",		//条件
+			"class.class_id asc", //並び替え
+
 		);
 		//順次取り出す
 		while($row = $this->fetch_assoc()){
@@ -295,53 +152,32 @@ class cmember extends crecord {
 	//--------------------------------------------------------------------------------------
 	/*!
 	@brief	指定されたIDの配列を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
 	@param[in]	$id		ID
 	@return	配列（1次元配列になる）空の場合はfalse
 	*/
 	//--------------------------------------------------------------------------------------
-	public function get_tgt($debug,$id){
-		if(!is_int($id)
+	public function get_tgt($id){
+		if(!cutil::is_number($id)
 		||  $id < 1){
 			//falseを返す
 			return false;
 		}
 		//親クラスのselect()メンバ関数を呼ぶ
 		$this->select(
-			$debug,			//デバッグ表示するかどうか
-			"member.*,prefecture.prefecture_name",			//取得するカラム
-			"member,prefecture",	//取得するテーブル
-			"member.member_id ={$id} and 
-member.prefecture_id = prefecture.prefecture_id"	//条件
-		);
-		return $this->fetch_assoc();
-	}
-	//--------------------------------------------------------------------------------------
-	/*!
-	@brief	フルーツとのマッチする配列を得る
-	@param[in]	$debug	デバッグ出力をするかどうか
-	@param[in]	$id		ID
-	@return	配列（1次元配列になる）
-	*/
-	//--------------------------------------------------------------------------------------
-	public function get_all_fruits_match($debug,$id){
-		$arr = array();
-		//親クラスのselect()メンバ関数を呼ぶ
-		$this->select(
-			$debug,			//デバッグ表示するかどうか
-			"*", //取得するカラム
-			"fruits_match",	//取得するテーブル
-			"member_id = {$id}", //条件
-			"fruits_id asc"	//並び替え
+			false,			//デバッグ表示するかどうか
+			"*",			//取得するカラム
+			"class,school",	//取得するテーブル
+			"class.school_id = school.school_id AND class.class_id=".$id,	//条件
+			"class.class_id asc"
 		);
 		//順次取り出す
 		while($row = $this->fetch_assoc()){
-			$arr[] = $row['fruits_id'];
+			$arr[] = $row;
 		}
 		//取得した配列を返す
 		return $arr;
-	}
 
+	}
 	//--------------------------------------------------------------------------------------
 	/*!
 	@brief	デストラクタ
@@ -353,4 +189,79 @@ member.prefecture_id = prefecture.prefecture_id"	//条件
 	}
 }
 
+//アカウントクラス
+class caccount extends crecord {
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	コンストラクタ
+	*/
+	//--------------------------------------------------------------------------------------
+	public function __construct() {
+		//親クラスのコンストラクタを呼ぶ
+		parent::__construct();
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	指定された範囲の配列を得る
+	@param[in]	$debug	デバッグ出力をするかどうか
+	@param[in]	$from	抽出開始行
+	@param[in]	$limit	抽出数
+	@return	配列（2次元配列になる）
+	*/
+	//--------------------------------------------------------------------------------------
+	public function get_all($column="account.*"){
+		$arr = array();
+		//親クラスのselect()メンバ関数を呼ぶ
+		$this->select(
+			false,			//デバッグ表示するかどうか
+			$column,			//取得するカラム
+			"account,class",	//取得するテーブル
+			"account.class_id=class.class_id",			//条件
+			"account.account_id asc"
+		);
+		//順次取り出す
+		while($row = $this->fetch_assoc()){
+			$arr[] = $row;
+		}
+		//取得した配列を返す
+		return $arr;
+	}	
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	デストラクタ
+	*/
+	//--------------------------------------------------------------------------------------
+	public function __destruct(){
+		//親クラスのデストラクタを呼ぶ
+		parent::__destruct();
+	}
+	//--------------------------------------------------------------------------------------
+	/*!
+	@brief	指定されたIDの配列を得る
+	@param[in]	$id		ID
+	@return	配列（1次元配列になる）空の場合はfalse
+	*/
+	//--------------------------------------------------------------------------------------
+	public function get_tgt($id){
+		if(!cutil::is_number($id)
+		||  $id < 1){
+			//falseを返す
+			return false;
+		}
+		//親クラスのselect()メンバ関数を呼ぶ
+		$this->select(
+			false,			//デバッグ表示するかどうか
+			"*",			//取得するカラム
+			"account,class",	//取得するテーブル
+			"account.class_id=class.class_id AND account.account_id=".$id,	//条件
+			"account.account_id asc"
+		);
+		//順次取り出す
+		while($row = $this->fetch_assoc()){
+			$arr[] = $row;
+		}
+		//取得した配列を返す
+		return $arr;
 
+	}
+}
